59cf8a0239f38b2cd79b01b03a1c0405be63773f
Denis Ravinski <delgoden@gmail.com>
2022-07-05 09:36:19 +0300

    completed homework according to the README.md

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,22 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	sem := make(chan struct{}, pool)
+	mu := &sync.Mutex{}
+	wg := &sync.WaitGroup{}
+
+	for i := int64(0); i < n; i++ {
+		wg.Add(1)
+		sem <- struct{}{}
+		go func(locker *sync.Mutex, wg *sync.WaitGroup, id int64) {
+			user := getOne(id)
+			locker.Lock()
+			res = append(res, user)
+			locker.Unlock()
+			<-sem
+			wg.Done()
+		}(mu, wg, i)
+	}
+	wg.Wait()
+	return res
 }
